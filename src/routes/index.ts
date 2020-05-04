b67a01e849fab9dbdc2d159ba540cb5cce34deda
Luiz Moraes <luizrmm97@gmail.com>
2020-05-03 23:14:10 -0300

    appointment complete

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import appoitmentsRouter from './appointments.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appoitmentsRouter);
+
+export default routes;
